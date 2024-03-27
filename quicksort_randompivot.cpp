a4bc5f9cc3114d36f84865b84835b52ce20eec77
THEO0808 <147980058+TeodorLepadatu@users.noreply.github.com>
2024-03-27 16:37:28 +0200

    l-am prins pe veric

--- quicksort_randompivot.cpp
+++ quicksort_randompivot.cpp
@@ -4,12 +4,17 @@
 #include <cstdlib>
 #include <ctime>
 #include <chrono>
+#include <random>
+#include <limits>
+
 std::ifstream fin("date.in");
 std::ofstream fout("date.out");
 
-int pivotrandom(std::vector<int>& arr, int left, int right) {
-    srand(time(0));
-    return arr[rand() % (right - left + 1) + left];
+int pivotrandom(int a,int b) {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dis(a, b);
+    return dis(gen);
 }
 
 std::vector<int> quicksort(std::vector<int>& arr)
@@ -20,7 +25,7 @@ std::vector<int> quicksort(std::vector<int>& arr)
     }
     else
     {
-        int pivot = pivotrandom(arr,0,arr.size()-1);  ///am ales un pivot random, o sa schimbam asta sau facem mai multe teste cu asta
+        int pivot = pivotrandom(0,arr.size()-1);  ///am ales un pivot random, o sa schimbam asta sau facem mai multe teste cu asta
         std::vector<int> left;
         std::vector<int> right;
         for (int i = 1; i < arr.size(); i++)    ///impart vectorul initial in doua astfel incat elementele din left sa fie mai mici decat pivotul si din right mai mare decat pivotul
@@ -44,6 +49,8 @@ std::vector<int> quicksort(std::vector<int>& arr)
 
 int main()
 {
+    int n;
+    fin>>n;
     std::vector<int> v;
     int num;
     while (fin >> num)
@@ -55,7 +62,11 @@ int main()
     //auto end = std::chrono::high_resolution_clock::now();
     //std::chrono::duration<double> duration = end - start;
     //std::cout << "Time taken: " << duration.count() << " seconds" << std::endl;
-    v = quicksort(v);   ///sortare
+    try {
+        v = quicksort(v);
+    } catch (const std::exception& e) {
+        return -1;
+    }
 
     for (int i = 0; i < v.size(); i++)
     {
