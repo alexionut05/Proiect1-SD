f48af215a95918b70cb696ccaf423464b3071cc3
THEO0808 <147980058+TeodorLepadatu@users.noreply.github.com>
2024-03-27 20:23:40 +0200

    Update nativesortpython.py

--- nativesortpython.py
+++ nativesortpython.py
@@ -3,9 +3,8 @@ g=open("output.txt", "w")
 n=f.readline()
 v=f.readline()
 arr=[int(x) for x in v.split()]
-v=arr
-v=sorted(v)
-for i in v:
+arr=sorted(arr)
+for i in arr:
     g.write(str(i)+" ")
 f.close()
 g.close()
