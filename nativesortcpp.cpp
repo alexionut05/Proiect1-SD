a8b5c0865a45f0f08a436c3c5ca68ac9356e2d5c
THEO0808 <147980058+TeodorLepadatu@users.noreply.github.com>
2024-03-27 20:28:15 +0200

    Create nativesortcpp.cpp

---
+++ nativesortcpp.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+void ReadArray(std::vector<float> &arr, size_t &n, char *input_file)
+{
+	std::ifstream in(input_file);
+
+	in >> n;
+	arr.resize(n);
+
+	for (size_t i = 0; i < n; ++i) {
+		in >> arr[i];
+	}
+
+	in.close();
+	return;
+}
+
+void PrintArray(std::vector<float> &arr, size_t &n, char *output_file)
+{
+	std::ofstream out(output_file);
+
+	for (size_t i = 0; i < n; ++i) {
+		out << arr[i] << ' ';
+	}
+
+	out.close();
+	return;
+}
+int main()
+{
+	size_t n;
+	std::vector<float> arr;
+	ReadArray(arr, n, argv[1]);
+	try {
+		sort(arr, arr + n);
+	} catch (const std::exception &e) {
+		return -1;
+	}
+    PrintArray(array, n, argv[2]);
+	return 0;
+}
