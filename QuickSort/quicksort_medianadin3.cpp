0c8173825061515387876a4a4026d8f0e84993a8
THEO0808 <147980058+TeodorLepadatu@users.noreply.github.com>
2024-03-28 20:27:07 +0200

    daca nu merge, ma bag in pat si nu ma mai ridic

--- QuickSort/quicksort_medianadin3.cpp
+++ QuickSort/quicksort_medianadin3.cpp
@@ -1,80 +1,61 @@
 #include <vector>
 #include <fstream>
-double medianadintrei(double a, double b, double c) {
-    if ((a < b && b < c) || (c < b && b < a))
-        return b;
-    else if ((b < a && a < c) || (c < a && a < b))
-        return a;
-    else
-        return c;
+#include <random>
+std::ifstream fin("date.in");
+std::ofstream fout("date.out");
+
+long long medianadintrei(std::vector<double>& arr, long long low, long long high) {
+    long long mid = low + (high - low) / 2;
+
+    //gasesc mijlocul dintre cele 3 valori
+    if (arr[low] > arr[mid])
+        std::swap(arr[low], arr[mid]);
+    if (arr[mid] > arr[high])
+        std::swap(arr[mid], arr[high]);
+    if (arr[low] > arr[mid])
+        std::swap(arr[low], arr[mid]);
+
+    return mid;
 }
-std::vector<double> quicksort(std::vector<double>& arr)
-{
-    if (arr.size() <= 1)
-    {
-        return arr;
-    }
-    else
-    {
-        double pivot;
-        double first = arr[0];
-        double middle = arr[arr.size() / 2];
-        double last = arr[arr.size() - 1];
-        pivot = medianadintrei(first, middle, last);  ///am ales un pivot random, o sa schimbam asta sau facem mai multe teste cu asta
-        std::vector<double> left;
-        std::vector<double> right;
-        for (long long i = 0; i < arr.size(); i++)    ///impart vectorul initial in doua astfel incat elementele din left sa fie mai mici decat pivotul si din right mai mare decat pivotul
-        {
-            if (arr[i] < pivot)
-            {
-                left.push_back(arr[i]);
-            }
-            else
-            {
-                right.push_back(arr[i]);
-            }
+long long partition(std::vector<double>& arr, long long low, long long high) {
+    long long pivotindex = medianadintrei(arr, low, high);
+    double pivot = arr[pivotindex];  //alegere pivot
+    long long i = low - 1;
+
+    for (long long j = low; j < high; j++) {
+        if (arr[j] <= pivot) {
+            i++;  // Incrementez indexul daca elementul e mai mic decat pivotul
+            std::swap(arr[i], arr[j]);
         }
-        left = quicksort(left);     ///apelez recursiv functia pentru a sorta cele doua subsiruri
-        right = quicksort(right);
-        left.push_back(pivot);
-        left.insert(left.end(), right.begin(), right.end());    ///reconstruiesc vectorul pentru a il afisa
-        return left;
+    }
+
+    std::swap(arr[i + 1], arr[high]);
+
+    return i + 1;  // returnez unde fac partitionarea
+}
+
+void quicksort(std::vector<double>& arr, long long low, long long high) {
+    if (low < high) {
+        long long part = partition(arr, low, high);
+
+        // Recursively sort elements before and after partition
+        quicksort(arr, low, part - 1);
+        quicksort(arr, part + 1, high);
     }
 }
 
-int main(int argc, char *argv[])
-{
-    if (argc != 3)
-    {
-        return 1;
-    }
-
-    std::ifstream fin(argv[1]);
-    std::ofstream fout(argv[2]);
-
-    std::vector<double> v;
+int main() {
+    std::vector<double> arr;
     double num;
-    double temp;
-    fin >> temp;
+    while(fin >> num)
+        arr.push_back(num);
 
-    while (fin >> num)
-    {
-        v.push_back(num);   ///input
+    quicksort(arr, 0, arr.size() - 1);
+
+    for (double x : arr) {
+        fout << x << " ";
     }
-
-    try {
-        v = quicksort(v);   ///sortare
-    } catch (std::exception& e) {
-        return -1;
-    }
-
-    for (long long i = 0; i < v.size(); i++)
-    {
-        fout << v[i] << " ";            ///afisare
-    }
-
-    fin.close();
-    fout.close();
 
     return 0;
 }
+
